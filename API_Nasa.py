f0818f34688ba0e9d4a6ca859847128955ca2cfd
Thanury Tanisue <thanurytanisue@gmail.com>
2025-12-16 22:53:57 -0300

    update: API da Nasa e Bitcoin

---
+++ API_Nasa.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY"
+
+response = requests.get(url) 
+
+print(response.json())
